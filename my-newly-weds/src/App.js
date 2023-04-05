3afc90f0124e46a93fd60604664432605fb57a68
Deorren Erive <erive92d@gmail.com>
2023-04-04 20:04:30 -0700

    added create react app with a test component

---
+++ my-newly-weds/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Test from "./Test";
+
+function App() {
+  return (
+    <div className="App">
+      {/* TESTING */}
+      <Test />
+    </div>
+  );
+}
+
+export default App;
