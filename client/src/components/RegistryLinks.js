5747d4b4d17af394efb850c02a81f6a03bda106b
slexicon <lexiscott0101@gmail.com>
2023-04-11 13:55:08 -0700

    update frontend styling

--- client/src/components/RegistryLinks.js
+++ client/src/components/RegistryLinks.js
@@ -1,4 +1,13 @@
 const RegistryLinks = () => {
-<h4 className="contentTitle">Traditional Registry Links</h4>
+    return(
+        <div>
+<h4 className="contentTitle" align = "center">Traditional Registry Links</h4>
+<div className="col-sm-12 col-lg-12 buttonContainer">
+<button className="btn"><a href="https://google.com">Crate & Barrel</a></button>
+<button className="btn"><a href="https://google.com">Patagonia</a></button>
+<button className="btn"><a href="https://google.com">Amazon</a></button>
+</div>
+</div>
+    )
 }
 export default RegistryLinks
