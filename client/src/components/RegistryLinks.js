f043b61d725305ec71dc8568cb7e1823f2bddc19
slexicon <lexiscott0101@gmail.com>
2023-04-11 18:57:51 -0700

    update front end colors

--- client/src/components/RegistryLinks.js
+++ client/src/components/RegistryLinks.js
@@ -3,9 +3,9 @@ const RegistryLinks = () => {
         <div>
 <h4 className="contentTitle" align = "center">Traditional Registry Links</h4>
 <div className="col-sm-12 col-lg-12 buttonContainer">
-<button className="btn"><a href="https://google.com">Crate & Barrel</a></button>
-<button className="btn"><a href="https://google.com">Patagonia</a></button>
-<button className="btn"><a href="https://google.com">Amazon</a></button>
+<button className="btn"><a href="https://google.com" target="_blank">Crate & Barrel</a></button>
+<button className="btn"><a href="https://google.com" target="_blank">Patagonia</a></button>
+<button className="btn"><a href="https://google.com" target="_blank">Amazon</a></button>
 </div>
 </div>
     )
