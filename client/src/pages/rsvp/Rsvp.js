817daa8b4c67537cbd9f13309d3a7913dd437435
slexicon <lexiscott0101@gmail.com>
2023-04-11 20:38:56 -0700

    more frontend fun

--- client/src/pages/rsvp/Rsvp.js
+++ client/src/pages/rsvp/Rsvp.js
@@ -1,5 +1,4 @@
 import PageHeader from "../../components/PageHeader";
-import SocialIcons from "../../components/SocialIcons";
 import RsvpForm from "../../components/RsvpForm";
 import RsvpInfo from "../../components/RsvpInfo";
 
@@ -14,7 +13,7 @@ const Rsvp = () => {
 
   return (
     <section className="contact">
-      <PageHeader title="RSVP" description="Let's Connect" />
+      <PageHeader title="RSVP" description="Please go ahead and" />
       <div className="contactWrap container">
         <div className="row">
           <div className="col-12 col-lg-6">
@@ -29,7 +28,6 @@ const Rsvp = () => {
           </div>
         </div>
       </div>
-      <SocialIcons />
     </section>
   );
 };
